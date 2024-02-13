12627d35e4324bada4c07edc9f2379860b4faa7d
maghje13 <96776599+maghje13@users.noreply.github.com>
2024-02-13 08:48:49 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+# this is a install script that will install the most common programming tools
+# this was created by @maghje13, you can find me on github :D
+
+# Text Editors
+sudo apt install nano -y
+sudo apt install neovim -y
+sudo apt install vim -y
+
+# Basic Tools (for managing your system and flexing)
+# Assuming you have git already (if not uncomment the line below)
+# sudo apt install git -y
+sudo apt install htop -y
+sudo apt install neofetch -y
+
+# Running/Compiling Programming Languages
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install node -y
+sudo apt install java -y
+sudo apt install gcc -y
+sudo apt install g++ -y
+sudo apt install php -y
+sudo apt install powershell -y
+sudo apt install golang -y
+sudo apt install rustc -y
+sudo apt install kotlin -y
+sudo apt install binutils -y
+sudo apt install perl -y
+sudo apt install haskell-platform -y
